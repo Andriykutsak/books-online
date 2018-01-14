bd7e642515cd7c3523d0cbb0d86be4f599d68a9f
Andriy Kutsak <andriykutsak.ak@gmail.com>
2018-01-14 20:54:03 +0300

    test

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Books online</title>
+	<!-- styles -->
+	<link rel="stylesheet" href="css/style.css">
+<!-- resset css -->
+<link rel="stylesheet" href="https://meyerweb.com/eric/tools/css/reset/reset200802.css">
+
+	<!--Google fonts -->
+
+</head>
+<body>
+	<!-- header -->
+	<header>
+		<!-- accaunt setting -->
+		<div class="head-menu ">
+			<div class="account-act">
+				
+			
+			<ul class="center">
+				<li><a href="#">Sing in</a></li>
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Order Status</a></li>
+				<li><a href="#">Help</a></li>
+			</ul></div>
+<!-- end accaunt setings -->
+
+			<div class="head-blocks">
+				<div class="center">
+					<div class="block-wrap">
+					
+				
+				<div class="logo"> <a href="index.php"><img src="img/logo.jpg" alt="logotype"></a></div>
+				
+					<div class="form-bg">
+						<form action="">
+							<input type="text" id="search-field">
+							<input type="submit" value="Search">
+							<div class="sch-ico"></div>
+						</form>
+					</div>
+				
+
+				<div class="cart-wrap">
+					<div class="cart-head">
+						<div class="cart-ico"></div>
+						<span class="cart-text">Your cart</span>
+						<span class="cart-items">(2 items)</span>
+					</div>
+					<div class="cart-footer">
+						<span class="cart-price">$125.0</span>
+						<div class="cart-btn">checkout</div>
+					</div>
+				</div>
+
+				<div class="wish-wrap">
+					<div class="whish-ico"></div>
+					<div class="wish-text">
+						Wish list
+						<div class="wish-num">20</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+		</div>
+		<!-- topm menu -->
+		<div class="top-menu-wrap">
+			<div class="center">
+				<ul class="top-menu">
+					<li><a href="#">computers</a></li>
+					<li><a href="#">cooking</a></li>
+					<li><a href="#">education</a></li>
+					<li><a href="#">fiction</a></li>
+					<li><a href="#">health</a></li>
+					<li><a href="#">mathematic</a></li>
+					<li><a href="#">medical</a></li>
+					<li><a href="#">reference</a></li>
+					<li><a href="#">sciens</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- top menu end -->
+	</header>
+	<!-- header end -->
+	<div class="content">
+		<div class="center">
+			<!-- sidebar -->
+			<div class="sidebar">
+				<div class="side-head">categories</div>
+<ul class="side-list">
+	<li><a href="#">all</a></li>
+	<div class="ganre">Fiction & literature</div>
+	<li><a href="#">children</a></li>
+	<li><a href="#">science</a></li>
+	<li><a href="#">fantasy</a></li>
+	<li><a href="#">mystery</a></li>
+	<li><a href="#">romance</a></li>
+	<li><a href="#">horror</a></li>
+	<li><a href="#">poetry</a></li>
+	<li><a href="#">literature</a></li>
+	<li><a href="#">crime</a></li>
+	<div class="ganre">Non-Fiction</div>
+	<li><a href="#">comic</a></li>
+	<li><a href="#">cook</a></li>
+	<li><a href="#">psychology</a></li>
+	<li><a href="#">medical</a></li>
+	<li><a href="#">art</a></li>
+	<li><a href="#">photography</a></li>
+	<li><a href="#">law</a></li>
+	<li><a href="#">history</a></li>
+	<li><a href="#">buisnes</a></li>
+	<li><a href="#">computer</a></li>
+	<div class="ganre">Other</div>
+	<li><a href="#">baby</a></li>
+	<li><a href="#">sex</a></li>
+	<li><a href="#">travel</a></li>
+	<li><a href="#">science</a></li>
+	<li><a href="#">sport</a></li>
+</ul>
+
+			</div>
+			<!-- sidebar end -->
+			<div class="products">
+				
+				<!-- navigation -->
+				<nav></nav>
+				<!-- navigation end -->
+			</div>
+		</div>
+	</div>
+	<!-- footer -->
+	<footer>
+		<div class="footer-bg">
+			<div class="center">
+				<div class="block ">
+				<h2 class="block-head">Biography & True Stories</h2>
+				<ul>
+				
+					<li><a href="#">General</a></li>
+					<li><a href="#">Diaries, Letters & Journals</a></li>
+					<li><a href="#">Memoirs</a></li>
+					<li><a href="#">True Stories</a></li>
+					<li><a href="#">Generic Exams</a></li>
+					<li><a href="#">GK Titles</a></li>
+					<li><a href="#">Medical Entrance</a></li>
+					<li><a href="#">Other Entrance Exams</a></li>
+					<li><a href="#">PG Entrance Examinations</a></li>
+					<li><a href="#">Self-help Titles</a></li>
+					<li><a href="#">Sociology</a></li>
+				</ul>
+			</div>
+			<div class="block">
+				<h2 class="block-head">Professional & Reference </h2>
+				<ul>
+					<li><a href="#">Academic and Reference</a></li>
+					<li><a href="#">Business Trade</a></li>
+					<li><a href="#">Engineering and Computer Science</a></li>
+					<li><a href="#">Humanities, Social Sciences and Languages</a></li>
+					<li><a href="#">Introduction to Computers</a></li>
+					<li><a href="#">Science and Maths</a></li>
+					<li><a href="#">Trade Business</a></li>
+					<li><a href="#">English Language & Literature</a></li>
+					<li><a href="#">English Language Teaching</a></li>
+					<li><a href="#">Environment Awareness</a></li>
+					<li><a href="#">Environment Protection </a></li>
+				</ul>
+			</div>
+			<div class="block">
+				<h2 class="block-head">Earth Sciences</h2>
+				<ul>
+					<li><a href="#">Earth Sciences</a></li>
+					<li><a href="#">Geography</a></li>
+					<li><a href="#">The Environment</a></li>
+					<li><a href="#">Regional & Area Planning
+</a></li>
+					<li><a href="#">Fantasy</a></li>
+					<li><a href="#">Gay</a></li>
+					<li><a href="#">Humorous</a></li>
+					<li><a href="#">Interactive</a></li>
+					<li><a href="#">Legal</a></li>
+					<li><a href="#">Lesbian</a></li>
+					<li><a href="#">Men'S Adventure</a></li>
+					
+				</ul>
+			</div>
+			<div class="block">
+				<h2 class="block-head">Mathematics</h2>
+				<ul>
+					<li><a href="#">Algebra</a></li>
+					<li><a href="#">Differential Equations</a></li>
+					<li><a href="#">Discrete Mathematics</a></li>
+					<li><a href="#">Fourier Analysis</a></li>
+					<li><a href="#">Numerical Analysis</a></li>
+					<li><a href="#">Probability</a></li>
+					<li><a href="#">Statistical Methods/data Analysis</a></li>
+					<li><a href="#">Stochastic And Random Processes</a></li>
+					<li><a href="#">Topology</a></li>
+					<li><a href="#">Statistics</a></li>
+					<li><a href="#">Mathematics</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="payment-block">
+			<div class="center">
+				<p class="payment-text"> We accept all major Credit Card/Debit Card/Internet Banking </p>
+			<ul>
+				<li><a href=""><img src="" alt=""></a></li>
+				<li><a href=""><img src="" alt=""></a></li>
+				<li><a href=""><img src="" alt=""></a></li>
+			</ul>
+			
+			<div class="privacy-block">
+			<p class="privacy-text">Conditions of Use Privacy Notice Interest-Based Ads © 1996-2013, Booksonline, Inc. or its affiliates 
+			</p>
+		</div>
+		</div>
+		</div>
+		
+			</div>
+			
+	</footer>
+	<!-- end footer -->
+</body>
+</html>
